7c94f4158075e1a19ee56cfa356dd2ad5e15d704
Sai <sai@MADHAVs-MBP.home>
2020-09-17 11:06:36 -0500

    vehicle addition

---
+++ src/app/shared/vehicle.ts
@@ -0,0 +1,8 @@
+export class Vehicle{
+    name: string;
+    model: string;
+    type: string;
+    year: number;
+    price: number;
+    destination_charges:number;
+}
--- src/app/vehicle-selection/vehicle-selection.component.ts
+++ src/app/vehicle-selection/vehicle-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Vehicle} from '../shared/vehicle';
 
 @Component({
   selector: 'app-vehicle-selection',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleSelectionComponent implements OnInit {
 
+  vehicles: Vehicle[];
+  
   constructor() { }
 
   ngOnInit(): void {
